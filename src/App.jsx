08bccbb6a9b5dc11827a7da9bbc94a22aa77d787
SamSamimz <samsamim595@gmail.com>
2026-03-12 01:33:23 +0600

    minor

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
+        <a href="https://github.com/SamSamimz/dev-ops" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
       </div>
